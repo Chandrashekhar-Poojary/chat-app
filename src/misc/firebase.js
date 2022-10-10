4e4755cee176883acddba221d88f0bfb9c58dcdd
chandra <poojarychandra1309@gmail.com>
2022-10-11 00:50:00 +0530

    initialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyCGPKXOK5e_GNRk0SJFJ0_BwMrogpoYbl0",
+  authDomain: "chat-web-app-ee5ec.firebaseapp.com",
+  databaseURL: "https://chat-web-app-ee5ec-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-ee5ec",
+  storageBucket: "chat-web-app-ee5ec.appspot.com",
+  messagingSenderId: "1091230451927",
+  appId: "1:1091230451927:web:cbcbffa3df8a60a2504892"
+};
+
+const app = firebase.initializeApp(config);
+
