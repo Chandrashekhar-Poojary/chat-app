ca336bf417b8e0db4e95384f875b037cfa490976
chandra <poojarychandra1309@gmail.com>
2022-10-11 15:34:25 +0530

    created editable input

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Drawer, Button} from 'rsuite';
+import {Drawer, Button, Divider} from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({onSignOut}) => {
 
   const { profile } = useProfile();
 
+  const onSave = async(newData) =>{
+    console.log(newData)
+  }
   return (
     <>
       <Drawer.Header>
@@ -16,6 +20,15 @@ const Dashboard = ({onSignOut}) => {
 
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider/>
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label = {<h6 className='mb-2'>Nick Name</h6>}
+        />
+
+        
       </Drawer.Body>
 
       <Drawer.Footer>
