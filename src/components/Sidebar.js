1370885f48876b598f7059ee4efa3e1ec09fb243
chandra <poojarychandra1309@gmail.com>
2022-10-11 20:21:02 +0530

    added new chat room

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateRoomBtnModal from './CreateRoomBtnModal'
 import DashboardToggle from './dashboard/DashboardToggle'
 
 const Sidebar = () => {
@@ -7,6 +8,7 @@ const Sidebar = () => {
 
     <div>
       <DashboardToggle/>
+      <CreateRoomBtnModal />
     </div>
 
     Bottom
