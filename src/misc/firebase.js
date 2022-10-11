81ff36e67d7daab705d7a62cbd67be23ffe223a2
chandra <poojarychandra1309@gmail.com>
2022-10-11 12:24:58 +0530

    added sign in page with FB and Google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,7 +1,8 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
-
-const config = {
+const firebaseConfig = {
   apiKey: "AIzaSyCGPKXOK5e_GNRk0SJFJ0_BwMrogpoYbl0",
   authDomain: "chat-web-app-ee5ec.firebaseapp.com",
   databaseURL: "https://chat-web-app-ee5ec-default-rtdb.firebaseio.com",
@@ -11,5 +12,8 @@ const config = {
   appId: "1:1091230451927:web:cbcbffa3df8a60a2504892"
 };
 
-const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const database = app.database();
+export { app, auth, database};
 
