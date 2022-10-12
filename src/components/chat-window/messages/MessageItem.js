f502006ace8fb1dac4d41ab4dd84e58892239f18
chandra <poojarychandra1309@gmail.com>
2022-10-13 02:32:03 +0530

    added button admin permission

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
-const MessageItem = ({message}) => {
+const MessageItem = ({message, handleAdmin}) => {
 
   const { author, createdAt, text} = message;
+
+  const isAdmin = useCurrentRoom(v =>v.isAdmin);
+  const admins = useCurrentRoom(v =>v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+
+  const isAuthor = auth.currentUser.uid === author.uid;
+
+  const canGrantAdmin = isAdmin && ! isAuthor;
+
+
   return (
     <li className='padded mb-1'>
       <div className='d-flex align-items-center font-bolder mb-1'>
@@ -14,7 +28,21 @@ const MessageItem = ({message}) => {
         <PresenceDot uid={author.uid}/>
         <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs"/>
       
-        <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black" />
+        <ProfileInfoBtnModal 
+            profile={author} 
+            appearance="link" 
+            className="p-0 ml-1 text-black"
+        >
+          {
+            canGrantAdmin &&
+            <Button block onClick={()=> handleAdmin(author.uid)} color="blue">
+                {isMsgAuthorAdmin 
+                  ? 'Remove Admin Permission' 
+                  : 'Give admin in this Room'}
+            </Button>
+          }
+          
+        </ProfileInfoBtnModal>
         <TimeAgo 
               datetime={ 
                 createdAt
@@ -29,4 +57,4 @@ const MessageItem = ({message}) => {
   )
 }
 
-export default MessageItem;
+export default memo(MessageItem);
