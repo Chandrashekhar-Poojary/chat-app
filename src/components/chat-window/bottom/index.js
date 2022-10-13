128021cd7ddb972c77219941fdf23cf52acde050
chandra <poojarychandra1309@gmail.com>
2022-10-13 11:31:50 +0530

    created attachment bottom with uploder component

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/compat/app';
 import { useProfile } from '../../../context/profile.context';
 import { useParams } from 'react-router';
 import { database } from '../../../misc/firebase';
+import AttachmentBtnModal from './AttachmentBtnModal';
 function assembleMessage(profile, chatId){
     return{
         roomId: chatId,
@@ -71,6 +72,7 @@ const Bottom = () => {
   return (
     <div>
         <InputGroup>
+            <AttachmentBtnModal />
             <Input 
                 placeholder="write a new message here..." 
                 value={input} 
